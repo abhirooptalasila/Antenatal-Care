366a9e7a829a952f0ac4018d622e59a503284497
Abhiroop Talasila <abhiroop.talasila@gmail.com>
2019-10-03 18:44:14 +0530

    Changes

--- Antenatal Care/LoginViewController.swift
+++ Antenatal Care/LoginViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //let vc = UIViewController()
+        //vc.modalPresentationStyle = .fullScreen //or .overFullScreen for transparency
+        //self.present(vc, animated: true, completion: nil)
+        
     }
 
 
---
+++ Antenatal Care/Patient.swift
@@ -0,0 +1,18 @@
+//
+//  Patient.swift
+//  Antenatal Care
+//
+//  Created by student on 03/10/19.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import Foundation
+
+class Patient {
+    var name:String
+    init(name:String="") {
+        self.name = name
+    }
+        
+    
+}
