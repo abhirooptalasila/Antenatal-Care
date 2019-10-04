7edfa1af254ebaebd6dcb1b2df367477c6a7b885
Rituza <ritujkalita1999@gmail.com>
2019-10-04 19:12:45 +0530

    Added multiple view Controllersss

---
+++ Antenatal Care/PastRecordsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PastRecordsViewController.swift
+//  Antenatal Care
+//
+//  Created by student on 04/10/19.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import UIKit
+
+class PastRecordsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Antenatal Care/TapNFCViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TapNFCViewController.swift
+//  Antenatal Care
+//
+//  Created by student on 04/10/19.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import UIKit
+
+class TapNFCViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
